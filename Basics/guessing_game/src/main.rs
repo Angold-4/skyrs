84216b9a4551d73e315c497612021826e0471f01
Angold-4 <wangold4w@gmail.com>
2022-07-02 16:35:05 +0800

    Finishing the first program -> guessing game !

--- Basics/guessing_game/src/main.rs
+++ Basics/guessing_game/src/main.rs
@@ -1,14 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // thread_rng gives the particular random number generator that we are going to use
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+
+        io::stdin() // returns an instance of std::io::Stdin
+            .read_line(&mut guess) // passing by reference
+            // readline puts whatever the user enters into the string guess
+            // and then return a Result->enumeration which is a type that can
+            // be in one of multiple possible states -> variantes (Err / Ok)
+            .expect("Failed to read line"); // expect is one of the method of type Result
+
+        // trim() removes the \n / whitespace around guess
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+            // "_" catchall value
+        };
+
+        println!("You guessed: {guess}"); // {} placeholder
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
